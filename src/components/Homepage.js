4dfbb1d4fe8d259eee853bf49d0d0c691b724bc0
Moon-Tae Kim <moon-tae@Moon-Taes-MacBook-Air.local>
2018-10-22 13:56:37 -0400

    10/22

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -20,6 +20,10 @@ export default class Homepage extends Component {
       <StyledHomepage>
         <StyledHeader size='huge'>Hello, I'm Moon-Tae </StyledHeader>
         <StyledSubHeader>Full Stack Developer | Musician</StyledSubHeader>
+        <div>
+        <i className="devicon-github-plain colored"></i>
+        <img src="https://www.edigitalagency.com.au/wp-content/uploads/new-linkedin-logo-png-transparent-background.png" alt="linkedin"/>
+        </div>
       </StyledHomepage>
     )
   }
